f5dd04356a35b50828e5b6d2ae017c04dfa78005
dipesh <jaswanidipesh8@gmail.com>
2024-02-04 21:26:07 +0530

    cors-fixed

--- server/app.js
+++ server/app.js
@@ -6,8 +6,8 @@ const app = express();
 
 const PORT = 8080 || process.env.PORT;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const chatRoutes = require("./routes/api");
 
